bd13e0c67df337a4e409be2d69af2a285585bbcb
james94 <jamesmedel94@gmail.com>
2019-03-12 21:06:04 -0700

    Trimmed dashboard routes list to 3 routes and 1 redirect

--- frontend/dashboard-react/src/routes/dashboard.jsx
+++ frontend/dashboard-react/src/routes/dashboard.jsx
@@ -2,15 +2,8 @@
 import Dashboard from "views/Dashboard/Dashboard.jsx";
 //import Sensors from "views/Sensors/Sensors.jsx";
 import Sensors from "views/Sensors/Sensors.jsx"
-
-import Icons from "views/Icons/Icons.jsx";
-import Notifications from "views/Notifications/Notifications.jsx";
-
-import Typography from "views/Typography/Typography.jsx";
-import TableList from "views/TableList/TableList.jsx";
-import Maps from "views/Maps/Maps.jsx";
-import Upgrade from "views/Upgrade/Upgrade.jsx";
-import UserPage from "views/UserPage/UserPage.jsx";
+//import SelfDriving from "views/SelfDriving/SelfDriving.jsx";
+import SelfDriving from "views/SelfDriving/SelfDriving.jsx";
 
 var dashRoutes = [
   {
@@ -25,41 +18,11 @@ var dashRoutes = [
     icon: "design_image", 
     component: Sensors 
   },
-  { 
-    path: "/lane-curvature", 
-    name: "Lane-Curvature", 
-    icon: "location_map-big", 
-    component: Maps 
-  },
-  {
-    path: "/behavior-cloning",
-    name: "Behavior-Cloning",
-    icon: "ui-1_bell-53",
-    component: Notifications
-  },
-  {
-    path: "/kalman-filters",
-    name: "Kalman-Filters",
-    icon: "users_single-02",
-    component: UserPage
-  },
-  {
-    path: "/localization",
-    name: "Localization",
-    icon: "files_paper",
-    component: TableList
-  },
-  {
-    path: "/path-planning",
-    name: "Path-Planning",
-    icon: "design-2_ruler-pencil",
-    component: Typography
-  },
   {
     path: "/self-driving",
     name: "Self-Driving",
     icon: "objects_spaceship",
-    component: Upgrade
+    component: SelfDriving
   },
   { redirect: true, path: "/", pathTo: "/dashboard", name: "Dashboard" }
 ];
