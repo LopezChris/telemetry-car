a4b5554837ea301df4aea430cf1c7139a389d625
james94 <jamesmedel94@gmail.com>
2019-03-15 18:46:25 -0700

    Added steps for launching frontend and backend web servers. Working on steps for launching both at same time.

--- README.md
+++ README.md
@@ -1,5 +1,5 @@
 # telemetry-car
 
-React.js and Node.js frameworks used to build telemetry web app that displays car sensor data
+**React.js** and **Express.js** frameworks used to build telemetry web app that displays car sensor data
 
-To launch the frontend react web application, go to **frontend/dashboard-react** folder's README.md file.
+Steps coming soon to launch Telemetry Web Application (frontend && backend).
--- setup.sh
+++ setup.sh
@@ -6,10 +6,5 @@ curl -sL https://deb.nodesource.com/setup_10.x | sudo -E bash -
 sudo apt-get install -y nodejs
 nodejs -v
 
-# Install React.js react-icons library inside base of react project with node_modules in same dir
-#cd frontend/dashboard-react
-#npm install react-icons --save
-
-
 # react plugin used to create icons
-#cimport { GiMovementSensor } from "react-icons/gi";
+#import { GiMovementSensor } from "react-icons/gi";
