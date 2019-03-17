5a4b2711c3af6c88b10ee1928e157bd95c2002a1
james94 <jamesmedel94@gmail.com>
2019-03-17 03:24:50 -0700

    Tested http get request for getting all camera frames and a single frame. Works successfully

--- backend/server.js
+++ backend/server.js
@@ -1,15 +1,14 @@
 const express = require('express');
 const app = express();
 const port = 4000;
-const fs = require('fs');
 
-let readCamFramesNames = fs.readdirSync("../frontend/src/assets/img/sensors/cam_frames");
-app.get('/', (req, res) => {
-    res.send(readCamFramesNames)
-});
+// app.use('/assets/data/input/sensors/cw_rs_tl/012919/cam_frames',
+//     express.static('./assets/data/input/sensors/cw_rs_tl/012919/cam_frames'));
 
-///../frontend/dashboard-react/src/assets/img/sensors
+// Create router to handle all requests beginning with /api/sensor/camera
+const cameraRouter = require('./src/routes/api/sensor/camera.js');
+app.use('/api/sensor/camera', cameraRouter);
 
 app.listen(port, () => {
-    console.log(`Example app listening on port ${port}!`)
+    console.log(`Example app listening on port ${port}!`);
 });
---
+++ backend/src/routes/api/sensor/camera.js
@@ -0,0 +1,24 @@
+const express = require('express');
+const fs = require('fs');
+
+let cameraFrames = [];
+cameraFrames = fs.readdirSync("src/assets/data/input/sensors/cw_rs_tl/012919/cam_frames/");
+//console.log(cameraFrames);
+
+const cameraRouter = express.Router();
+
+// Created route for cameraRouter that'll send all cameraFrames for a GET request
+cameraRouter.get('/', (req, res, next) => {
+    res.send(cameraFrames);
+});
+
+// Created route for cameraRouter that'll send a cameraFrame for a GET request
+// When a GET /cameraFrames/1 request arrives,
+// cameraRouter.get('/:id') matches full path /cameraFrames/1
+// so, the callback is invoked and cameraFrame with index id is fetched from 
+// cameraFrames array and sent back
+cameraRouter.get('/:id', (req, res, next) => {
+    res.send(cameraFrames[req.params.id]);    
+});
+
+module.exports = cameraRouter;
