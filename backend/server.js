267c6a85f6a3c360fde5269ddc676226c7b4a127
james94 <jamesmedel94@gmail.com>
2019-03-18 01:35:34 -0700

    Added API for lidar data

--- backend/server.js
+++ backend/server.js
@@ -7,9 +7,13 @@ const port = 4000;
 // Root dir from which to serve static assets
 app.use(express.static(path.join(__dirname, 'src/assets/data')));
 
-// Create router to handle all requests beginning with /api/sensor/camera
+// Create camerRouter to handle all requests beginning with /api/sensor/camera
 const cameraRouter = require('./src/routes/api/sensor/camera.js');
 app.use('/api/sensor/camera', cameraRouter);
+
+// Create lidarRouter to handle all requests beginning with /api/sensor/lidar
+const lidarRouter = require('./src/routes/api/sensor/lidar.js');
+app.use('/api/sensor/lidar', lidarRouter);
 
 app.listen(port, () => {
     console.log(`Example app listening on port ${port}!`);
