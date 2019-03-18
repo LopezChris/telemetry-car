9e983e8bc39b55d7575bbeb91023aafe9974e49d
james94 <jamesmedel94@gmail.com>
2019-03-17 18:26:51 -0700

    Added serving static files to server.js app

--- backend/server.js
+++ backend/server.js
@@ -1,9 +1,11 @@
 const express = require('express');
+const path = require('path');
 const app = express();
 const port = 4000;
 
-// app.use('/assets/data/input/sensors/cw_rs_tl/012919/cam_frames',
-//     express.static('./assets/data/input/sensors/cw_rs_tl/012919/cam_frames'));
+// Use absolute path of directory you want to serve
+// Root dir from which to serve static assets
+app.use(express.static(path.join(__dirname, 'src/assets/data')));
 
 // Create router to handle all requests beginning with /api/sensor/camera
 const cameraRouter = require('./src/routes/api/sensor/camera.js');
